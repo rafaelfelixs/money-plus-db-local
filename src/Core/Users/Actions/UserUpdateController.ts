d8b42934e21d2f033a6d1b3562d1e86c3601843c
Janaina-Costa <janainacosta.dev@gmail.com>
2024-06-27 03:32:22 -0300

    feat: Update UserUpdateController to handle user updates by ID

--- src/Core/Users/Actions/UserUpdateController.ts
+++ src/Core/Users/Actions/UserUpdateController.ts
@@ -1,22 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { UserUpdateService } from '../Services/UserUpdateService';
-import { Users } from '../../../Infraestructure/Entities/Users';
 
 export class UserUpdateController {
   constructor(private readonly service: UserUpdateService) {}
 
   public async handle(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const { id } = req.param;
-      const user: Users = req.body;
-      if (user.userName) {
-        const updatedUser = await this.service.invoke(id, {
-          ...user,
-          userName: user.userName,
-        });
-        res.status(200).send(updatedUser);
-      }
+      const { id } = req.params;
+      const userName = req.body;
+
+      //TODO: Adicionar validação
+      const updatedUser = await this.service.invoke(id, userName);
+
+      res.status(200).send(updatedUser);
     } catch (error) {
       next(error);
     }
