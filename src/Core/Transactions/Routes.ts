ac8efc996c22273a24f65a3a798f30f5bb48fe7a
rafaelfs <rafaelfelix.santos@gmail.com>
2024-07-03 08:06:16 +0800

    feat: adding service transaction

--- src/Core/Transactions/Routes.ts
+++ src/Core/Transactions/Routes.ts
@@ -1,10 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { transactionCreateController } from './index';
+import { transactionCreateController, transactionListController } from './index';
 
 const routerTransactions = express.Router();
 
 routerTransactions.post('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
   return transactionCreateController.handle(req, res, next);
+});
+
+routerTransactions.get('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
+  return transactionListController.handle(req, res, next);
 });
 
 export { routerTransactions };
