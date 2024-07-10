ca67c506e336a89ae3649a51494376367fe5b44f
Jaqueline <133174675+jueslyyy@users.noreply.github.com>
2024-07-10 20:55:36 -0300

    adiciona delete,update

--- src/Core/Transactions/Routes.ts
+++ src/Core/Transactions/Routes.ts
@@ -1,10 +1,23 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { transactionCreateController } from './index';
+import { transactionCreateController, transactionStatusUpdateController, transactionDeleteController } from './index';
+
 
 const routerTransactions = express.Router();
 
 routerTransactions.post('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
   return transactionCreateController.handle(req, res, next);
+});
+
+routerTransactions.put('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
+  return transactionStatusUpdateController.handle(req, res, next);
+});
+
+routerTransactions.delete('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
+  return transactionDeleteController.handle(req, res, next);
+});
+
+routerTransactions.get('/v1/transactions', (req: Request, res: Response, next: NextFunction) => {
+  return res.send('entrei');
 });
 
 export { routerTransactions };
--- src/Core/Transactions/index.ts
+++ src/Core/Transactions/index.ts
@@ -1,9 +1,18 @@
+
 import { UserStorage } from '../Users/Storage/UserStorage';
 import { TransactionStorage } from './Storage/TransactionStorage';
 import { TransactionCreateController } from './Actions/TransactionCreateController';
 import { TransactionCreateService } from './Services/TransactionCreateService';
+import { TransactionStatusUpdateController } from './Actions/TransactionStatusUpdateController';
+import { TransactionStatusUpdateService } from './Services/TransactionStatusUpdateService';
+import { TransactionDeleteController }  from './Actions/TransactionDeleteController'
+import { TransactionDeleteService } from './Services/TransactionDeleteService';
+
+
 
 export const userStorage = new UserStorage();
 export const transactionStorage = new TransactionStorage();
 
 export const transactionCreateController: TransactionCreateController = new TransactionCreateController(new TransactionCreateService(userStorage, transactionStorage));
+export const transactionStatusUpdateController: TransactionStatusUpdateController = new TransactionStatusUpdateController(new TransactionStatusUpdateService(userStorage, transactionStorage)); 
+export const transactionDeleteController: TransactionDeleteController = new TransactionDeleteController(new TransactionDeleteService(userStorage, transactionStorage)); 
