165e900fdd78ea7a85ca6c4820d5ca4d42178a31
Janaina-Costa <janainacosta.dev@gmail.com>
2024-07-13 20:11:22 -0300

    feat: Add transaction update endpoint and controller

--- src/Core/Transactions/Routes.ts
+++ src/Core/Transactions/Routes.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { transactionByIdController, transactionCreateController } from './index';
+import { transactionByIdController, transactionCreateController, transactionUpdateController } from './index';
 
 const routerTransactions = express.Router();
 
@@ -9,6 +9,10 @@ routerTransactions.post('/v1/transactions', (req: Request, res: Response, next: 
 
 routerTransactions.get('/v1/transactions/:id', (req: Request, res: Response, next: NextFunction) => {
   return transactionByIdController.handle(req, res, next);
+});
+
+routerTransactions.put('/v1/transactions/:id', (req: Request, res: Response, next: NextFunction) => {
+  return transactionUpdateController.handle(req, res, next);
 });
 
 export { routerTransactions };
--- src/Core/Transactions/index.ts
+++ src/Core/Transactions/index.ts
@@ -4,9 +4,12 @@ import { TransactionCreateController } from './Actions/TransactionCreateControll
 import { TransactionCreateService } from './Services/TransactionCreateService';
 import { TransactionByIdController } from './Actions/TransactionByIdController';
 import { TransactionByIdService } from './Services/TransactionByIdService ';
+import { TransactionUpdateController } from './Actions/TransactionUpdateController';
+import { TransactionUpdateService } from './Services/TransactionUpdateervice';
 
 export const userStorage = new UserStorage();
 export const transactionStorage = new TransactionStorage();
 
 export const transactionCreateController: TransactionCreateController = new TransactionCreateController(new TransactionCreateService(userStorage, transactionStorage));
 export const transactionByIdController: TransactionByIdController = new TransactionByIdController(new TransactionByIdService(transactionStorage));
+export const transactionUpdateController: TransactionUpdateController = new TransactionUpdateController(new TransactionUpdateService(transactionStorage));
